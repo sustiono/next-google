4e67a5d61c0c14141b9982ffc1447355660d1a5e
Tio Lampard <sustiono19@gmail.com>
2021-04-24 15:53:42 +0700

    Add Search Page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
+import { useRef } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import Avatar from "../components/Avatar";
-import Footer from "../components/Footer";
+import { useRouter } from "next/router";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import { SearchIcon } from "@heroicons/react/outline";
 
+import Avatar from "../components/Avatar";
+import Footer from "../components/Footer";
+
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <Head>
@@ -25,11 +40,14 @@ export default function Home() {
 
           <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
 
-          <Avatar url='https://lh3.googleusercontent.com/ogw/ADGmqu_IK-sDxmI8w0NscwuaTeX2ERnzqBkmuZS6m4c5DA=s32-c-mo' />
+          <Avatar url='https://avatars.githubusercontent.com/u/19564599?v=4' />
         </div>
       </header>
 
-      <form className='flex flex-col items-center mt-44 flex-grow w-4/5'>
+      <form
+        className='flex flex-col items-center mt-44 flex-grow w-4/5'
+        onSubmit={search}
+      >
         <Image
           src='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
           width={300}
@@ -37,7 +55,11 @@ export default function Home() {
         />
         <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
           <SearchIcon className='h-5 mr-3 text-gray-500' />
-          <input type='text' className='flex-grow focus:outline-none' />
+          <input
+            ref={searchInputRef}
+            type='text'
+            className='flex-grow focus:outline-none'
+          />
           <MicrophoneIcon className='h-5' />
         </div>
 
---
+++ pages/search.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Search = () => {
+  return (
+    <div>
+      <Head>
+        <title>Search Results</title>
+      </Head>
+    </div>
+  );
+};
+
+export default Search;
