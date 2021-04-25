1db696af4fc8b4b0d7c5205b5a2234ae738873f9
Tio Lampard <sustiono19@gmail.com>
2021-04-25 14:39:12 +0700

    Add pagination

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,3 +1,5 @@
+import PaginationButtons from "./PaginationButtons";
+
 const SearchResults = ({ results }) => {
   return (
     <div className='w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52 '>
@@ -22,6 +24,8 @@ const SearchResults = ({ results }) => {
           <p className='line-clamp-2'>{result.snippet}</p>
         </div>
       ))}
+
+      <PaginationButtons />
     </div>
   );
 };
