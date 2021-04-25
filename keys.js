a78ff5a0130700524dd63bb6c72baed2614a2a22
Tio Lampard <sustiono19@gmail.com>
2021-04-25 15:28:46 +0700

    Move Keys to ENV

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyDXmTszYjvGwuP5c1dtHLR99QBTYqLvrOs";
+export const API_KEY = process.env.API_KEY;
 
 // https://cse.google.com/cse/create
-export const CONTEXT_KEY = "d9f14ef909d529468";
+export const CONTEXT_KEY = process.env.CONTEXT_KEY;
